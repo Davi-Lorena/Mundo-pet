9eb0d71f75ea2efd3bb2de1582add2fd64069b3d
Davi Lorena <davilorena2016@gmail.com>
2024-11-29 11:50:13 -0300

    adding assests in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+
 
 module.exports = {
 target: "web",
@@ -20,8 +22,20 @@ mode: "development",
         liveReload: true,
     },
 
-plugins: [new HTMLWebpackPlugin({
+plugins: [
+    new HTMLWebpackPlugin({
     template: path.resolve(__dirname, "index.html"),
-}),],
+}),
+
+new copyWebpackPlugin({
+    patterns: [ 
+     {    
+        from: path.resolve(__dirname, "src", "assets"),
+        to: path.resolve(__dirname, "dist", "src", "assets")
+     },
+    ]
+})
+
+],
 
 }
