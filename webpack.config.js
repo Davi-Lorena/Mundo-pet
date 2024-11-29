90f44cf837b2c9bab3fe1793ab3c7dee84e68faa
Davi Lorena <davilorena2016@gmail.com>
2024-11-29 12:03:10 -0300

    installing babel

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,16 @@ module: {
     test: /\.css$/i,
     use: ["style-loader", "css-loader"],
 },
+{
+test: /\.js$/i,
+exclude: /node_modules/,
+use: {
+    loader: "babel-loader",
+    options: {
+        presets: ["@babel/preset-env"]
+    },
+},
+},
     ],
 },
 
